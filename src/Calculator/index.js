3b4b1207f73efa01df9c2071f0ca1773b253e1a9
Sujay Alaspure <sujay.alaspure@robosoftin.com>
2022-04-27 22:03:17 +0530

    completed calc

---
+++ src/Calculator/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { BoxWrapper, Button, ButtonWrapper, CalculatorWrapper, Display, OperandsWrapper } from "./style";
+
+function Calculator() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const getButtons = () => {
+    let buttons = [];
+    for (let i = 1; i < 10; i++) {
+      buttons.push(
+        <Button onClick={() => onClick(i)} key={i}>
+          {i}
+        </Button>
+      );
+    }
+    return buttons;
+  };
+
+  const ops = ["+", "-", "*", "/", "."];
+  const onClick = (value) => {
+    if ((ops.includes(value) && input === "") || (ops.includes(value) && ops.includes(input.slice(-1)))) return;
+    setInput((prev) => prev + value);
+    console.log(value);
+    if (!ops.includes(value)) setResult(eval(input + value));
+  };
+  const onSubmit = (e) => {
+    console.log(input);
+    setInput(eval(input).toString());
+  };
+
+  const del = () => {
+    if (!input) return;
+    const value = input.slice(0, -1);
+    setInput(value.toString());
+  };
+
+  return (
+    <CalculatorWrapper>
+      <BoxWrapper>
+        <Display>
+          <span>({result || "0"})</span> {input || "0"}
+        </Display>
+        <OperandsWrapper>
+          {ops.map((operator, index) => (
+            <Button onClick={() => onClick(operator)} key={index}>
+              {operator}
+            </Button>
+          ))}
+        </OperandsWrapper>
+        <ButtonWrapper>
+          {getButtons()}
+          <Button onClick={() => onClick("0")}>0</Button>
+          <Button onClick={onSubmit}> {"="}</Button>
+          <Button onClick={del}>DEL</Button>
+        </ButtonWrapper>
+      </BoxWrapper>
+    </CalculatorWrapper>
+  );
+}
+
+export default Calculator;
---
+++ src/Calculator/style.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+const colors = {
+  background: "#023047",
+  secondary: "#8ecae6",
+  text: "#bde0fe",
+};
+
+export const CalculatorWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+`;
+
+export const BoxWrapper = styled.div`
+  background-color: #fff;
+  min-height: 100px;
+  min-width: 300px;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  /* padding: 0.5rem; */
+`;
+
+export const Display = styled.div`
+  background-color: ${colors.secondary};
+  color: ${colors.background};
+  font-size: 2rem;
+  padding: 0.5rem;
+  text-align: right;
+  span {
+    font-size: 1.2rem;
+    opacity: 0.8;
+  }
+`;
+export const OperandsWrapper = styled.div`
+  background-color: ${colors.background};
+  color: ${colors.text};
+  display: flex;
+  border-bottom: 1px solid ${colors.secondary};
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  background-color: ${colors.secondary};
+`;
+
+export const Button = styled.button`
+  flex: 1 1 33.33%;
+  max-width: 33.33%;
+  outline: none;
+  padding: 0.75rem;
+  appearance: none;
+  border: none;
+  background-color: transparent;
+  /* background-color: ${colors.background}; */
+  color: inherit;
+  font-size: 1.2rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 0.4s;
+
+  &:hover {
+    opacity: 0.8;
+    background-color: ${colors.text};
+    color: ${colors.background};
+  }
+`;
